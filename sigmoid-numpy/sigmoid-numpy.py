9e66ed38e8cb543d77304b873cb269f846778061
Vũ Văn Tới <161419450+toivuvan@users.noreply.github.com>
2026-09-05 15:09:47 +0700

    Solve sigmoid-numpy

---
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def sigmoid(x: list | float) -> np.ndarray | float:
+    """
+    Returns the sigmoid value for a scalar or each element of a list.
+    """
+    # Write code here
+    arr = np.asarray(x, dtype = float)
+    return 1 / (1 + np.exp(-arr))
+    pass
